f8e133e5ab8826b571d137faead972de38648a10
Sarah Liu <swliu79@uw.edu>
2018-04-09 17:15:30 -0700

    Got stuck on the largest grant receiver question

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -4,33 +4,33 @@
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-
+gates_money <- read.csv('data/gates_money.csv', stringsAsFactors = FALSE)
 
 # Use the View function to look at the loaded data
-
+View(gates_money)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
-
+organization <- gates_money$organization
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
 # This is a useful debugging tip if you hit errors later!
-
+is.vector(organization)
 
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
-
+mean_grant <- mean(gates_money$total_amount)
 
 # What was the dollar amount of the largest grant?
-
+largest_grant <- max(gates_money$total_amount)
 
 # What was the dollar amount of the smallest grant?
-
+smallest_grant <- min(gates_money$total_amount)
 
 # Which organization received the largest grant?
-
+largest_receiver <- organization(gates_money$total_amount == largest_grant)
 
 # Which organization received the smallest grant?
 
